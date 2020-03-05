f75baf48160eb13088c0eb191ca073f546512b1c
skeptical.knowledge <skeptical.knowledge@users.noreply.github.com>
2020-03-05 14:26:44 +0100

    działa zapis i klasa pojazd

---
+++ katalog-samochodowy-cs/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace katalog_samochodowy_cs
+{
+    class Program
+    {
+
+        
+        static void Main(string[] args)
+        {
+            Pojazd nowyPojazd = new Pojazd("Opel","Vectra",2000,1.9f,2331,'A');
+            Rejestr.lista.Add(nowyPojazd);
+            Rejestr.lista[0].Wypisz();
+            Rejestr.ZapiszZawartoscKataloguDoPliku();
+        }
+    }
+
+
+}
---
+++ katalog-samochodowy-cs/Rejestr.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+
+namespace katalog_samochodowy_cs
+{
+    class Rejestr
+    {
+        public static List<Pojazd> lista = new List<Pojazd>();
+        static string nazwaPliku = "katalogcs.txt";
+        public static void ZapiszZawartoscKataloguDoPliku()
+        {
+            FileStream plik = new FileStream(nazwaPliku, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            StreamWriter strumienZapisu = new StreamWriter(plik);
+            for( int i = 0; i < Rejestr.lista.Count; i++)
+            {
+                strumienZapisu.WriteLine(Rejestr.lista[i].marka + " " + Rejestr.lista[i].model + " " + Rejestr.lista[i].pojemnosc + " " + Rejestr.lista[i].przebieg + " " + Rejestr.lista[i].rocznik + " " + Rejestr.lista[i].typSkrzyniBiegow);
+            }
+            strumienZapisu.Close();
+        }
+        public static void WczytajZawartoscKataloguZPliku()
+        {
+
+        }
+
+    }
+}
